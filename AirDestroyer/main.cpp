f359683bc839d3a0debd1e8b78f0ba95e050db3b
Damir564 <ddamir.nizamutdinov@yandex.ru>
2024-01-23 23:34:29 +0300

    Add project files.

---
+++ AirDestroyer/main.cpp
@@ -0,0 +1,46 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "Header.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        getHeader();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void getHeader()
+{
+    std::cout << "Header" << std::endl;
+}
